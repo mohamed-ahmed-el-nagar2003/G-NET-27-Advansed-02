a9bb237111b276195a767ca8df24e04db51d6a91
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-03-26 17:18:30 +0200

    Task 01 : Smart Product Search

---
+++ Delegates Assignment_02 Advansed/Product .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Assignment_02_Advansed
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; } // "Electronics", "Clothing", "Food", "Books" 
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
--- Delegates Assignment_02 Advansed/Program.cs
+++ Delegates Assignment_02 Advansed/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-           
+
+//            List<Product> catalog = new()
+//{
+//    new Product { Id=1, Name="Laptop", Category="Electronics", Price=1200, Stock=10 },
+//    new Product { Id=2,Name="Phone", Category="Electronics", Price=800, Stock=25 },
+//    new Product { Id=3, Name="T-Shirt", Category="Clothing", Price=30, Stock=100 },
+//    new Product { Id=4, Name="Jeans", Category="Clothing", Price=60, Stock=50 },
+//    new Product { Id=5, Name="Chocolate", Category="Food", Price=5, Stock=200 },
+//    new Product { Id=6, Name="Coffee Beans", Category="Food", Price=15, Stock=80 },
+//    new Product { Id=7, Name="C# Book", Category="Books", Price=45, Stock=30 },
+//    new Product { Id=8, Name="Novel", Category="Books", Price=20, Stock=60 },
+//    new Product { Id=9,Name="Headphones", Category="Electronics", Price=150, Stock=60 }};
+           // Console.WriteLine("============================Electronics===============================");
+           // Func<Product,bool> conditionOfElectronics = product => product.Category == "Electronics" ;
+           //List<Product> ElectronicProducts = SearchProducts.Search(catalog, conditionOfElectronics);
+           // foreach (Product product in ElectronicProducts)
+           // {
+           //     Console.WriteLine($"{product.Name}-{product.Price}({product.Stock})");
+           // }
+
+           // Console.WriteLine("============================cheaper than 50$===============================");
+
+           // Func<Product, bool> cheaperthan = product => product.Price < 50;
+           // List<Product> Cheaper = SearchProducts.Search(catalog, cheaperthan);
+           // foreach (Product product in Cheaper)
+           // {
+           //     Console.WriteLine($"{product.Name}-{product.Price}({product.Stock})");
+           // }
+
+           // Console.WriteLine("============================in stock (Stock > 0) ===============================");
+
+
+           //      Func<Product, bool> inStock = product => product.Stock > 0;
+           // List<Product> InStock = SearchProducts.Search(catalog, inStock);
+           // foreach (Product product in InStock)
+           // {
+           //     Console.WriteLine($"{product.Name}-{product.Price}({product.Stock})");
+           // }
+
+           // Console.WriteLine("============================ Clothing products under $100 ===============================");
+           //        Func<Product, bool> Clothing = product => product.Category == "Clothing";
+           // List<Product> Clothes = SearchProducts.Search(catalog, Clothing);
+           // foreach (Product product in Clothes)
+           // {
+           //     Console.WriteLine($"{product.Name}-{product.Price}({product.Stock})");
+           // }
+
+
         }
+
+
     }
+
 }
