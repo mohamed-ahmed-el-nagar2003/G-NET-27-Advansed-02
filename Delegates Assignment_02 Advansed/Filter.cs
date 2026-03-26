f8f457efc2863969250bb40dc17d4b3b4ac05b72
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-03-26 20:47:31 +0200

    3.3. Filter Products

---
+++ Delegates Assignment_02 Advansed/Filter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Assignment_02_Advansed
+{
+    internal class Filter
+    {
+        public static List<Product> FilterStock(List<Product> products, Predicate<Product> pre)
+        {
+            List<Product> result = new List<Product>();
+            foreach (var item in products)
+            {
+                if (true)
+                {
+                    result.Add(item);
+                }
+
+            }
+            return result;
+        }
+    }
+}
--- Delegates Assignment_02 Advansed/Program.cs
+++ Delegates Assignment_02 Advansed/Program.cs
@@ -96,6 +96,17 @@
             //    Console.WriteLine(item);
             //}
             #endregion
+
+            #region 3.3. Filter Products 
+            //Console.WriteLine("======================Check Stock=====================");
+            //Predicate<Product> filter = Product => Product.Stock < 20 ? true : false;
+            //List<Product> FilterStock = Filter.FilterStock(catalog, filter);
+            //foreach (var item in FilterStock)
+            //{
+            //    Console.WriteLine($"[Low Stock]{item.Name} : only {item.Stock} Left !");
+            //}
+            #endregion
+
             #endregion
 
 
